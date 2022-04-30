8e9ea13fe8ac0afe87230a25eb5f313186fc68f1
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-04-30 09:53:36 +0300

    added a seed for db users

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,20 +2,17 @@
 @section('content')
 
 <div class="d-flex justify-content-center login">
-    <form>
+    <form method="post" action="{{'/login'}}">
+        @csrf
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1"  class="form-label">Password</label>
+            <input type="password" class="form-control" name="password" id="exampleInputPassword1">
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input col-6"  id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Login</button>
     </form>
 </div>
 
