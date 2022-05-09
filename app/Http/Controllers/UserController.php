9f2d6f5b23f633e43893cbbc197855b5f13d0cee
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-05-09 05:48:58 +0300

    login with error handles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function login(Request $request){
 
-//        return $request->input();
 
-        return User::where(['email'=>$request->email])->first();
+
+        $users = User::where(['email'=>$request->email])->first();
+        $password = User::where(['password'=>$request->password])->first();
+
+
+        if (!$users|| !Hash::check($request->password,$users->password)){
+            return  "Usere does not exists";
+        }else {
+
+            $request->session()->put('user',$request);
+            return redirect('/product');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
+Route::get('/', function () {
     return view('login');
 });
 Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
+
+Route::get('/product',[\App\Http\Controllers\ProductController::class,'index']);
